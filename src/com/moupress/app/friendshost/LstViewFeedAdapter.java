6da66ded2afd86ed6fb20c1a24e0b21d0fc9fcf1
LJ <aleremail@gmail.com>
2012-04-13 23:23:43 +0800

    Recycle the listview Item

--- src/com/moupress/app/friendshost/LstViewFeedAdapter.java
+++ src/com/moupress/app/friendshost/LstViewFeedAdapter.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 
 import com.github.droidfu.widgets.WebImageView;
 import com.moupress.app.friendshost.sns.FeedItem;
+import com.moupress.app.friendshost.uicomponent.FeedItemUIComponent;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
@@ -61,117 +62,184 @@ public class LstViewFeedAdapter extends BaseAdapter{
 		// TODO Auto-generated method stub
 		return position;
 	}
-
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-
-		if (convertView == null) {
-			convertView = viewInflator.inflate(iLayoutResId, null);
-		}
-		
-		WebImageView img_Head = (WebImageView) convertView.findViewById(R.id.img_feeduser);
-		String sHeadImgSrc = feedArrayList.get(position).getzFriend().getHeadurl();
-		if(sHeadImgSrc != null)
-		{
-			img_Head.setImageUrl(sHeadImgSrc);
-			img_Head.loadImage();
-		}
-//		Bitmap imgHead = null; 
-//		if (sHeadImgSrc != null) {
-//			imgHead = ImageOperations(sHeadImgSrc);
-//		}
-//		if (imgHead != null) {
-//			img_Head.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-//			img_Head.setImageBitmap(imgHead);
-//		}
-		
-		TextView txv_FeedUser = (TextView) convertView.findViewById(R.id.txt_name);
-		
-		if (feedArrayList == null || feedArrayList.size() < 2) {
-			txv_FeedUser.setText("Feed Not available yet. \nPlease refresh later");
-			
-			//disable other views
-			convertView.findViewById(R.id.img_feeduser).setVisibility(View.GONE);
-			((LinearLayout)convertView.findViewById(R.id.layout_img)).setVisibility(View.GONE);
-			convertView.findViewById(R.id.txt_msgcreatedtime).setVisibility(View.GONE);
-			convertView.findViewById(R.id.txt_msgbody).setVisibility(View.GONE);
-		} else {
-
-			//compulsory field
-			//TextView txv_FeedUser = (TextView) convertView.findViewById(R.id.txt_name);
-			txv_FeedUser.setText(feedArrayList.get(position).getsName());
-			TextView txv_MsgCreationTime = (TextView) convertView.findViewById(R.id.txt_msgcreatedtime);
-			String sCreateTime = feedArrayList.get(position).getsCreatedTime();
-			txv_MsgCreationTime.setText(sCreateTime);
 	
-			// optional field
-			// message or tagged story
-			String sMsgBody = feedArrayList.get(position).getsMsgBody();
-			String sStory = feedArrayList.get(position).getsStory();
-			TextView txv_MsgBody = (TextView) convertView.findViewById(R.id.txt_msgbody);
-
-			if ( sMsgBody != null && sStory != null ) {
-				txv_MsgBody.setText(sMsgBody + "\n" + sStory);
-			} else if ( sMsgBody != null ) {
-				txv_MsgBody.setText(sMsgBody);
-			} else if ( sStory != null ) {
-				txv_MsgBody.setText(sStory);
-			} else {
-				txv_MsgBody.setVisibility(View.GONE);
-			}
-			String username = feedArrayList.get(position).getsName();
-//			if (username.equals("梁玉萍")) {
-//				System.out.println("");
-//			}
-			WebImageView img_PhotoPreview = (WebImageView) convertView.findViewById(R.id.img_photopreview);
-			String sImgSrc = feedArrayList.get(position).getsPhotoPreviewLink();
-			//img_PhotoPreview.setImageUrl(sHeadImgSrc);
-			//img_PhotoPreview.
-			//img_PhotoPreview.loadImage();
-			if (sImgSrc != null && sImgSrc.startsWith("http://") && sImgSrc.endsWith(".jpg")) {
-				img_PhotoPreview.setImageUrl(sImgSrc);
-				//img_PhotoPreview.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-				img_PhotoPreview.loadImage();
-			} else {
-				img_PhotoPreview.setVisibility(View.GONE);
-			}
-			//String sImgSrc = "http://photos-g.ak.fbcdn.net/hphotos-ak-ash4/431333_10150739807624187_554329186_11304408_1165959123_s.jpg";
-//			Bitmap imgPhoto = ImageOperations(sImgSrc);
-//			if (imgPhoto != null) {
-//				img_PhotoPreview.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-//				img_PhotoPreview.setImageBitmap(imgPhoto);
-//			} else {
-//				img_PhotoPreview.setVisibility(View.GONE);
-//			}
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent)
+	{
+		FeedItemUIComponent feedItemUIComponent;
+		if(convertView == null)
+		{
+			convertView = viewInflator.inflate(iLayoutResId, null);
+			feedItemUIComponent = new FeedItemUIComponent();
 			
-			// Image related text
-			String sImgName = feedArrayList.get(position).getsPhotoPreviewName();
-			String sImgCaption = feedArrayList.get(position).getsPhotoPreviewCaption();
-			String sImgDescription = feedArrayList.get(position).getsPhotoPreviewDescription();
+			//Img_Head
+			feedItemUIComponent.setImg_Head((WebImageView) convertView.findViewById(R.id.img_feeduser));
 			
-			TextView txv_ImgName = (TextView) convertView.findViewById(R.id.txv_imgName);
-			TextView txv_ImgCaption = (TextView) convertView.findViewById(R.id.txv_imgCaption);
-			TextView txv_ImgDecription = (TextView) convertView.findViewById(R.id.txv_imgdescription);
-			if (sImgName != null) {
-				txv_ImgName.setText(sImgName);
-			} else {
-				txv_ImgName.setVisibility(View.GONE);
-			}
-			if (sImgCaption != null) {
-				txv_ImgCaption.setText(sImgCaption);
-			} else {
-				txv_ImgCaption.setVisibility(View.GONE);
-			}
-			if (sImgDescription != null) {
-				txv_ImgDecription.setText(sImgDescription);
-//			} else if (sMsgBody != null && sStory != null ) {
-//				txv_ImgDecription.setText(sStory);
-			} else {
-				txv_ImgDecription.setVisibility(View.GONE);
-			}
+			//txv_FeedUser
+			feedItemUIComponent.setTxv_FeedUser((TextView) convertView.findViewById(R.id.txt_name));
+			
+			//layout_img
+			feedItemUIComponent.setImg_Layout((LinearLayout)convertView.findViewById(R.id.layout_img));
+			
+			//txt_msgcreatedtime
+			feedItemUIComponent.setTxv_MsgCreationTime((TextView)convertView.findViewById(R.id.txt_msgcreatedtime));
+			
+			//txt_msgbody
+			feedItemUIComponent.setTxv_MsgBody((TextView)convertView.findViewById(R.id.txt_msgbody));
+			
+			//img_photopreview
+			feedItemUIComponent.setImg_PhotoPreview((WebImageView) convertView.findViewById(R.id.img_photopreview));
+			
+			//txv_imgName
+			feedItemUIComponent.setTxv_ImgName((TextView) convertView.findViewById(R.id.txv_imgName));
+			
+			//txv_ImgCaption
+			feedItemUIComponent.setTxv_ImgCaption((TextView) convertView.findViewById(R.id.txv_imgCaption));
+			
+			//txv_ImgDecription
+			feedItemUIComponent.setTxv_ImgDecription((TextView) convertView.findViewById(R.id.txv_imgdescription));
+			
+			convertView.setTag(feedItemUIComponent);
+		}
+			else
+		{
+			feedItemUIComponent = (FeedItemUIComponent) convertView.getTag();
+		}
+		
+		feedItemUIComponent.ImgHeadLoad(feedArrayList.get(position).getzFriend().getHeadurl());
+		if (feedArrayList == null || feedArrayList.size() < 2) {
+			
+			feedItemUIComponent.getTxv_FeedUser().setText("Feed Not available yet. \nPlease refresh later");
+			feedItemUIComponent.getImg_Head().setVisibility(View.GONE);
+			feedItemUIComponent.getTxv_MsgCreationTime().setVisibility(View.GONE);
+			feedItemUIComponent.getTxv_MsgBody().setVisibility(View.GONE);
+			
+		}else {
+			
+			feedItemUIComponent.getTxv_FeedUser().setText(feedArrayList.get(position).getsName());
+			feedItemUIComponent.getTxv_MsgCreationTime().setText(feedArrayList.get(position).getsCreatedTime());
+			feedItemUIComponent.TxtMsgBodyLoad(feedArrayList.get(position).getsMsgBody(), feedArrayList.get(position).getsStory());
+			
+			feedItemUIComponent.ImgPhotoPreviewLoad(feedArrayList.get(position).getsPhotoPreviewLink());
+			
+			feedItemUIComponent.TxtImgNameLoad(feedArrayList.get(position).getsPhotoPreviewName());
+			feedItemUIComponent.TxvImgCaptionLoad( feedArrayList.get(position).getsPhotoPreviewCaption());
+			feedItemUIComponent.TxvImgDecription(feedArrayList.get(position).getsPhotoPreviewDescription());
+			
 		}
 		return convertView;
 	}
+
+//	@Override
+//	public View getView(int position, View convertView, ViewGroup parent) {
+//
+//		if (convertView == null) {
+//			convertView = viewInflator.inflate(iLayoutResId, null);
+//		}
+//		
+//		WebImageView img_Head = (WebImageView) convertView.findViewById(R.id.img_feeduser);
+//		String sHeadImgSrc = feedArrayList.get(position).getzFriend().getHeadurl();
+//		if(sHeadImgSrc != null)
+//		{
+//			img_Head.setImageUrl(sHeadImgSrc);
+//			img_Head.loadImage();
+//		}
+////		Bitmap imgHead = null; 
+////		if (sHeadImgSrc != null) {
+////			imgHead = ImageOperations(sHeadImgSrc);
+////		}
+////		if (imgHead != null) {
+////			img_Head.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+////			img_Head.setImageBitmap(imgHead);
+////		}
+//		
+//		TextView txv_FeedUser = (TextView) convertView.findViewById(R.id.txt_name);
+//		
+//		if (feedArrayList == null || feedArrayList.size() < 2) {
+//			txv_FeedUser.setText("Feed Not available yet. \nPlease refresh later");
+//			
+//			//disable other views
+//			convertView.findViewById(R.id.img_feeduser).setVisibility(View.GONE);
+//			((LinearLayout)convertView.findViewById(R.id.layout_img)).setVisibility(View.GONE);
+//			convertView.findViewById(R.id.txt_msgcreatedtime).setVisibility(View.GONE);
+//			convertView.findViewById(R.id.txt_msgbody).setVisibility(View.GONE);
+//		} else {
+//
+//			//compulsory field
+//			//TextView txv_FeedUser = (TextView) convertView.findViewById(R.id.txt_name);
+//			txv_FeedUser.setText(feedArrayList.get(position).getsName());
+//			TextView txv_MsgCreationTime = (TextView) convertView.findViewById(R.id.txt_msgcreatedtime);
+//			String sCreateTime = feedArrayList.get(position).getsCreatedTime();
+//			txv_MsgCreationTime.setText(sCreateTime);
+//	
+//			// optional field
+//			// message or tagged story
+//			String sMsgBody = feedArrayList.get(position).getsMsgBody();
+//			String sStory = feedArrayList.get(position).getsStory();
+//			TextView txv_MsgBody = (TextView) convertView.findViewById(R.id.txt_msgbody);
+//
+//			if ( sMsgBody != null && sStory != null ) {
+//				txv_MsgBody.setText(sMsgBody + "\n" + sStory);
+//			} else if ( sMsgBody != null ) {
+//				txv_MsgBody.setText(sMsgBody);
+//			} else if ( sStory != null ) {
+//				txv_MsgBody.setText(sStory);
+//			} else {
+//				txv_MsgBody.setVisibility(View.GONE);
+//			}
+//			String username = feedArrayList.get(position).getsName();
+////			if (username.equals("梁玉萍")) {
+////				System.out.println("");
+////			}
+//			WebImageView img_PhotoPreview = (WebImageView) convertView.findViewById(R.id.img_photopreview);
+//			String sImgSrc = feedArrayList.get(position).getsPhotoPreviewLink();
+//			//img_PhotoPreview.setImageUrl(sHeadImgSrc);
+//			//img_PhotoPreview.
+//			//img_PhotoPreview.loadImage();
+//			if (sImgSrc != null && sImgSrc.startsWith("http://") && sImgSrc.endsWith(".jpg")) {
+//				img_PhotoPreview.setImageUrl(sImgSrc);
+//				//img_PhotoPreview.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+//				img_PhotoPreview.loadImage();
+//			} else {
+//				img_PhotoPreview.setVisibility(View.GONE);
+//			}
+//			//String sImgSrc = "http://photos-g.ak.fbcdn.net/hphotos-ak-ash4/431333_10150739807624187_554329186_11304408_1165959123_s.jpg";
+////			Bitmap imgPhoto = ImageOperations(sImgSrc);
+////			if (imgPhoto != null) {
+////				img_PhotoPreview.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+////				img_PhotoPreview.setImageBitmap(imgPhoto);
+////			} else {
+////				img_PhotoPreview.setVisibility(View.GONE);
+////			}
+//			
+//			// Image related text
+//			String sImgName = feedArrayList.get(position).getsPhotoPreviewName();
+//			String sImgCaption = feedArrayList.get(position).getsPhotoPreviewCaption();
+//			String sImgDescription = feedArrayList.get(position).getsPhotoPreviewDescription();
+//			
+//			TextView txv_ImgName = (TextView) convertView.findViewById(R.id.txv_imgName);
+//			TextView txv_ImgCaption = (TextView) convertView.findViewById(R.id.txv_imgCaption);
+//			TextView txv_ImgDecription = (TextView) convertView.findViewById(R.id.txv_imgdescription);
+//			if (sImgName != null) {
+//				txv_ImgName.setText(sImgName);
+//			} else {
+//				txv_ImgName.setVisibility(View.GONE);
+//			}
+//			if (sImgCaption != null) {
+//				txv_ImgCaption.setText(sImgCaption);
+//			} else {
+//				txv_ImgCaption.setVisibility(View.GONE);
+//			}
+//			if (sImgDescription != null) {
+//				txv_ImgDecription.setText(sImgDescription);
+////			} else if (sMsgBody != null && sStory != null ) {
+////				txv_ImgDecription.setText(sStory);
+//			} else {
+//				txv_ImgDecription.setVisibility(View.GONE);
+//			}
+//		}
+//		return convertView;
+//	}
 
 	public void addItem(String[] feedMsg) {
 		FeedItem item = new FeedItem();
@@ -215,6 +283,8 @@ public class LstViewFeedAdapter extends BaseAdapter{
 		}
 		return bmp;
 	}
+
+
 
 
 //	public Object fetch(String address) throws MalformedURLException,IOException {
---
+++ src/com/moupress/app/friendshost/uicomponent/FeedItemUIComponent.java
@@ -0,0 +1,150 @@
+package com.moupress.app.friendshost.uicomponent;
+
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.github.droidfu.widgets.WebImageView;
+
+public class FeedItemUIComponent {
+
+	private WebImageView img_Head;
+	private TextView txv_FeedUser;
+	private TextView txv_MsgCreationTime;
+	private TextView txv_MsgBody;
+	private WebImageView img_PhotoPreview;
+	
+	private TextView txv_ImgName;
+	private TextView txv_ImgCaption;
+	private TextView txv_ImgDecription;
+	private LinearLayout img_Layout;
+	
+	public WebImageView getImg_Head() {
+		return img_Head;
+	}
+	public void setImg_Head(WebImageView img_Head) {
+		this.img_Head = img_Head;
+	}
+	public TextView getTxv_FeedUser() {
+		return txv_FeedUser;
+	}
+	public void setTxv_FeedUser(TextView txv_FeedUser) {
+		this.txv_FeedUser = txv_FeedUser;
+	}
+	public TextView getTxv_MsgCreationTime() {
+		return txv_MsgCreationTime;
+	}
+	public void setTxv_MsgCreationTime(TextView txv_MsgCreationTime) {
+		this.txv_MsgCreationTime = txv_MsgCreationTime;
+	}
+	public TextView getTxv_MsgBody() {
+		return txv_MsgBody;
+	}
+	public void setTxv_MsgBody(TextView txv_MsgBody) {
+		this.txv_MsgBody = txv_MsgBody;
+	}
+	public WebImageView getImg_PhotoPreview() {
+		return img_PhotoPreview;
+	}
+	public void setImg_PhotoPreview(WebImageView img_PhotoPreview) {
+		this.img_PhotoPreview = img_PhotoPreview;
+	}
+	public TextView getTxv_ImgName() {
+		return txv_ImgName;
+	}
+	public void setTxv_ImgName(TextView txv_ImgName) {
+		this.txv_ImgName = txv_ImgName;
+	}
+	public TextView getTxv_ImgCaption() {
+		return txv_ImgCaption;
+	}
+	public void setTxv_ImgCaption(TextView txv_ImgCaption) {
+		this.txv_ImgCaption = txv_ImgCaption;
+	}
+	public TextView getTxv_ImgDecription() {
+		return txv_ImgDecription;
+	}
+	public void setTxv_ImgDecription(TextView txv_ImgDecription) {
+		this.txv_ImgDecription = txv_ImgDecription;
+	}
+	
+	
+	public LinearLayout getImg_Layout() {
+		return img_Layout;
+	}
+	public void setImg_Layout(LinearLayout img_Layout) {
+		this.img_Layout = img_Layout;
+	}
+	
+	public void ImgHeadLoad(String sHeadImgSrc)
+	{
+		if(sHeadImgSrc != null)
+		{
+			img_Head.setImageUrl(sHeadImgSrc);
+			img_Head.loadImage();
+		}
+	}
+	
+	public void FeedUserLoad(String txtFeedUser)
+	{
+		this.txv_FeedUser.setText(txtFeedUser);
+	}
+	
+	public void TxtCreationTimeLoad(String txv_MsgCreationTime)
+	{
+		this.txv_MsgCreationTime.setText(txv_MsgCreationTime);
+	}
+	
+	public void TxtMsgBodyLoad(String sMsgBody,String sStory )
+	{
+		if ( sMsgBody != null && sStory != null ) {
+			txv_MsgBody.setText(sMsgBody + "\n" + sStory);
+		} else if ( sMsgBody != null ) {
+			txv_MsgBody.setText(sMsgBody);
+		} else if ( sStory != null ) {
+			txv_MsgBody.setText(sStory);
+		} else {
+			txv_MsgBody.setVisibility(View.GONE);
+		}
+	}
+	
+	public void ImgPhotoPreviewLoad(String sImgSrc)
+	{
+		if (sImgSrc != null && sImgSrc.startsWith("http://") && sImgSrc.endsWith(".jpg")) {
+			img_PhotoPreview.setImageUrl(sImgSrc);
+			//img_PhotoPreview.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+			img_PhotoPreview.loadImage();
+		} else {
+			img_PhotoPreview.setVisibility(View.GONE);
+		}
+	}
+	
+	public void TxtImgNameLoad(String sImgName)
+	{
+		if (sImgName != null) {
+			txv_ImgName.setText(sImgName);
+		} else {
+			txv_ImgName.setVisibility(View.GONE);
+		}
+	}
+	
+	public void TxvImgCaptionLoad(String sImgCaption)
+	{
+		if (sImgCaption != null) {
+			txv_ImgCaption.setText(sImgCaption);
+		} else {
+			txv_ImgCaption.setVisibility(View.GONE);
+		}
+	}
+	
+	public void TxvImgDecription(String sImgDescription)
+	{
+		if (sImgDescription != null) {
+			txv_ImgDecription.setText(sImgDescription);
+//		} else if (sMsgBody != null && sStory != null ) {
+//			txv_ImgDecription.setText(sStory);
+		} else {
+			txv_ImgDecription.setVisibility(View.GONE);
+		}
+	}
+}
