36ec5eafe26818154bb00f112d5527ef1836fc2f
LJ <aleremail@gmail.com>
2012-04-27 23:53:43 +0800

    style and string for title bars

---
+++ src/com/moupress/app/friendshost/ui/UIManager.java
@@ -0,0 +1,13 @@
+package com.moupress.app.friendshost.ui;
+
+import android.app.Activity;
+
+public class UIManager {
+
+	private Activity zActivity;
+	
+	public UIManager(Activity zActivity)
+	{
+		this.zActivity = zActivity;
+	}
+}
