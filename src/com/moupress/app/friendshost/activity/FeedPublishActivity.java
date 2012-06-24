ca54c6f58f9d50fa2b77b0830a089717fd0d48f4
Winston Liu <cq01.liu@gmail.com>
2012-06-24 23:31:16 +0800

    Merge

--- src/com/moupress/app/friendshost/activity/FeedPublishActivity.java
+++ src/com/moupress/app/friendshost/activity/FeedPublishActivity.java
@@ -14,6 +14,7 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Display;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
